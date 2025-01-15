729412fee2e3265c753f23f4d3000256fdf86d0a
Sahatsawat Kanpai <keyboard2543@gmail.com>
2025-01-15 21:38:06 +0700

    ✨ address are able to store the string value on the contract

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Web3 } from 'web3';
+import Web3 from 'web3';
 
-const web3 = new Web3('http://localhost:7545');
-
-// For more methods: https://docs.web3js.org/libdocs/Web3Eth
+const web3 = new Web3(window.ethereum); // Dynamically use MetaMask provider
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'keyboard2543dapps-ui';
+  web3: Web3;
+  contract: any;
   blockNumber = BigInt(-1);
   chainId = BigInt(-1);
-  contractAddress = "0x9E3241EA3d969f2351A76E3E45B911845Ed62215";
+  contractAddress = "0xa5C7AC4a51A7a0954cA3b81CA94dF1f553Be2B9E";
   contractABI = [
     {
       "anonymous": false,
@@ -122,23 +122,70 @@ export class AppComponent implements OnInit {
     }
   ];
 
-  async ngOnInit() {
-    this.blockNumber = await web3.eth.getBlockNumber();
-    this.chainId = await web3.eth.getChainId();
-  }
-
   showingBalance = BigInt(-1);
-  async getBalance() {
-    const balance = await web3.eth.getBalance(this.showingAccounts);
-    console.log(balance);
-    this.showingBalance = BigInt(balance);
+  showingAccounts = "0x0";
+  showingStoredString = "";
+  stringToStore = "";
+
+  constructor() {
+    this.web3 = new Web3(window.ethereum); // Use MetaMask's provider
   }
 
-  // login with metamask
-  showingAccounts = "0x0";
+  async ngOnInit() {
+    try {
+      await this.initializeContract();
+      this.blockNumber = BigInt(await this.web3.eth.getBlockNumber());
+      this.chainId = BigInt(await this.web3.eth.getChainId());
+      console.log("Initialized successfully!");
+    } catch (error) {
+      console.error("Initialization failed:", error);
+    }
+  }
+
+  async initializeContract() {
+    this.contract = new this.web3.eth.Contract(this.contractABI, this.contractAddress);
+  }
+
   async login() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    this.showingAccounts = accounts[0];
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      this.showingAccounts = accounts[0];
+      console.log("Logged in:", this.showingAccounts);
+      this.showingBalance = BigInt(await this.web3.eth.getBalance(this.showingAccounts));
+      this.fetchString();
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }
+
+  async getBalance() {
+    try {
+      const balance = await this.web3.eth.getBalance(this.showingAccounts);
+      this.showingBalance = BigInt(balance);
+      console.log("Balance fetched:", this.showingBalance);
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  }
+
+  async storeString() {
+    try {
+      await this.contract.methods['storeValue'](this.stringToStore).send({ from: this.showingAccounts });
+      console.log("String stored successfully!");
+      this.fetchString();
+    } catch (error) {
+      console.error("Error storing string:", error);
+    }
+  }
+
+  async fetchString() {
+    try {
+      const value: string = await this.contract.methods['getValue']().call({ from: this.showingAccounts });
+      this.showingStoredString = value;
+      console.log("Fetched string:", this.showingStoredString);
+    } catch (error) {
+      console.error("Error fetching string:", error);
+    }
   }
 }
 
